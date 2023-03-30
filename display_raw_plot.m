79e5615428f82734a288a6fae1dc29f46bb8b4b7
estellerv <estelle.herve@univ-amu.fr>
2023-03-30 10:57:34 +0200

    Update display_raw_plot.m

--- display_raw_plot.m
+++ display_raw_plot.m
@@ -1,4 +1,4 @@
-function display_raw_plot(indir,subject_of_interest)
+%function display_raw_plot(indir,subject_of_interest)
 %%Script for opening bdf files and display plot of raw data
 
 indir = '\\Filer\home\Invites\herve\Mes documents\These\EEG\Data\DEVLANG_data';
@@ -8,7 +8,9 @@ indir = '\\Filer\home\Invites\herve\Mes documents\These\EEG\Data\DEVLANG_data';
 
 [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
 
-subject_of_interest = ['DVL_027_T24'];
+subject_of_interest = {'DVL_007_T8', 'DVL_008_T10', 'DVL_018_T8', 'DVL_029_T10', 'DVL_032_T10', 'DVL_021_T18'} ;
+% subject_of_interest = {'DVL_003_T10', 'DVL_003_T6', 'DVL_007_T8', 'DVL_008_T10', 'DVL_018_T8', 'DVL_029_T10', 'DVL_032_T10', 'DVL_021_T18'} ;
+% subject_of_interest = {'DVL_007_T6'} ;
 
 % Reads all folders that are in indir 
 d = dir(indir); 
@@ -16,19 +18,18 @@ isub = [d(:).isdir]; % returns logical vector if is folder
 subjects = {d(isub).name}';
 subjects(ismember(subjects,{'.','..'})) = []; % Removes . and ..
 
-
 %Loop through subjects
-    %for jj=1:length(subjects)
-    for jj=find(ismember(subjects, subject_of_interest))  
-        
-     fprintf(strcat(subjects{jj}, '...\n'));
+    for ind = 1:length(subject_of_interest)
+     
+    subject = subject_of_interest{ind} ;
+    fprintf(strcat(subject, '...\n'));
      
     %% IMPORT
     % Get BDF file
-    fname= dir(fullfile(indir,subjects{jj},'*.bdf'));
+    fname= dir(fullfile(indir,subject,'*.bdf'));
  
     % Select bdf file in the folder
-    EEG = pop_biosig(fullfile(indir, subjects{jj}, fname.name));
+    EEG = pop_biosig(fullfile(indir, subject, fname.name));
     
     % Save a first dataset in EEGLAB 
     [~,filename,~] = fileparts(fname.name);    
@@ -37,6 +38,10 @@ subjects(ismember(subjects,{'.','..'})) = []; % Removes . and ..
     EEG = eeg_checkset( EEG );
     pop_eegplot( EEG, 1, 1, 1);
 
-    end
+    eeglab redraw ;
     
-end
+    end
+
+    
+    
+%end
