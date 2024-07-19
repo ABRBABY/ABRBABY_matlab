056049a34150cb71d28e3c071940827cb275f971
Anne-Sophie Dubarry <anne-sophie.dubarry@univ-amu.fr>
2024-07-19 13:21:34 +0200

    Debug test existence of BT toolbox dir + subject in list force_rerun

--- test_existance_of_BT_toolbox.m
+++ test_existance_of_BT_toolbox.m
@@ -1,4 +1,4 @@
-function [flag_sub_to_create] = test_existance_of_BT_toolbox(OPTIONS)
+function [flag_sub_to_create] = test_existance_of_BT_toolbox(OPTIONS,output_suffix)
 % ERPs sanity check script - 
 % Estelle Herve, A.-Sophie Dubarry - 2023 - %80PRIME Project
 
@@ -11,6 +11,6 @@ subjects(ismember(subjects,{'.','..'})) = []; % Removes . and ..
 %Loop through subjects
 for jj=1:length(subjects) 
 
-    flag_sub_to_create(jj) = ~exist(fullfile(OPTIONS.indir,subjects{jj},'BT_toolbox_formatted'),'dir'); 
+    flag_sub_to_create(jj) = ~isempty(dir(fullfile(OPTIONS.indir,subjects{jj},'BT_toolbox_formatted',strcat('*',output_suffix,'*')))); 
       
 end
